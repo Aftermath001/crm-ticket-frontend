2f62fc482d93408e5fafc06502e1544d7298e32a
Aftermath001 <alvinasiachi@gmail.com>
2024-05-08 09:31:21 +0300

    Navbar

---
+++ src/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './partials/Header'
+import Footer from './partials/Footer'
+
+function Layout() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
